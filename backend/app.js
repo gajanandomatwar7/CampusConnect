8b91b6826ca536e748bb57980a7e44fc9431903d
gajanandomatwar7 <gajanandomatwar1@gmail.com>
2026-03-26 16:46:55 +0530

    Basic UI structure

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ app.use('/',uiRoutes);
 
 app.set('view engine', 'ejs');
 
+app.use(express.static("public"));
 
 
 app.listen(3000,()=>{
---
+++ backend/public/JS/home.js
@@ -0,0 +1,8 @@
+/*
+js for home page 
+*/
+
+function login(role) {
+  // redirect with query param
+  window.location.href = `/login?role=${role}`;
+}
