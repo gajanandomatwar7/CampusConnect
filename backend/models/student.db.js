2ab7728726c0bdb149b0b7b862242989d4da95db
MadMaxAxe <anuj.jundhare@walchandsangli.ac.in>
2026-03-28 02:16:52 +0530

    DB Schema

--- backend/models/student.db.js
+++ backend/models/student.db.js
@@ -5,3 +5,38 @@
 
 
 //write function inside config folder to connect with mongo db atlas 
+const mongoose=require('mongoose')
+
+const StudentSchema= new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+        required: true,
+    },
+    PRN:{
+        type: Number,
+        required: true,
+        unique: true,
+        validate:function(n){
+            return Number.isInteger(n) && n>=100000000 && n<=999999999;
+        },
+        message: props => `${props.value} is not a valid PRN!`
+    },
+    dept:{
+        type:String,
+        required: true,
+    },
+    semster:{
+        type: Number,
+        required:true,
+        validate:function(n){
+            return Number.isInteger(n) && n>=8 && n<=3;
+        },
+        message: props=>`${props.value} is not a valid Semester!`
+    }
+})
+
+module.exports=mongoose.model("StudentDB",StudentSchema);
