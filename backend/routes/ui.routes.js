1b15138abd91d244ea065a0b53f3f758383875a1
Anuj <anujsjm14@gmail.com>
2026-03-29 16:19:25 +0530

    ui routes rectified

--- backend/routes/ui.routes.js
+++ backend/routes/ui.routes.js
@@ -3,6 +3,15 @@ const uiRoutes=express.Router();
 
 uiRoutes.get('/',(req,res)=>{
     res.render('home');
+});
+
+uiRoutes.get('/login',(req,res)=>{
+    const role=req.query.role;
+    res.render('login',{role})
+})
+
+uiRoutes.get('/studentRegister',(req,res)=>{
+    res.render('studentRegister')
 })
 
 module.exports=uiRoutes;
